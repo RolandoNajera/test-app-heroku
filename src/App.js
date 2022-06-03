6a557b02783d94dac49cd7aebf9165dc7e028f4d
Rolando Najera Valdez <v-rovaldez@expediagroup.com>
2022-06-03 18:35:50 -0500

    adding fetch server

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 const { render } = require("react-dom");
 
 const App = () => {
+
+    const [resp, setResp] = useState("");
+
+    useEffect(() => {
+        getRequest();
+    }, []);
+
+    const getRequest = async () => {
+        const fetchResponse = await fetch('https://test-server-heroku.herokuapp.com');
+        const response = await fetchResponse.text();
+        console.log(response);
+        setResp(response);
+    }
+
     return (
       <div>
         <h1>Adopt Me!</h1>
+        {resp}
       </div>
     );
   };
